e701af48bcdebf28036b882d7d8c0afa9f3556b6
= <jakub.madejczyk92@gmail.com>
2017-07-20 18:09:20 +0100

    CSS Slide, flip Trans, Hide

--- src/app/components/Flight.js
+++ src/app/components/Flight.js
@@ -1,10 +1,50 @@
 import React from 'react'
 
 export class Flight extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            slide: false,
+            flip: false
+        }
+    }
+
+    onFlip(){
+        this.setState({
+            slide: false,
+            flip: true
+        })
+    }
+
+    onSlide(){
+        this.setState({
+            slide: true,
+            flip: false
+        })
+    }
+
     render(){
         return (
-            <div>
-                <h3>Flight</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="s12">
+
+                    </div>
+                </div>
+                <div className="row">
+                    <div className=".s8 offset-s2 center align">
+                        <div className={"card deep-purple z-depth-2 "+ (this.state.slide ? 'slide' : '') + (this.state.flip ? 'flip' : '')} >
+                            <div className="card-content white-text">
+                                <span className="card-title">Flight</span>
+                                <p>CSS Animation</p>
+                            </div>
+                            <div className="card-action">
+                                <a onClick={this.onFlip.bind(this)} style={{cursor: 'pointer'}}>Flip</a>
+                                <a onClick={this.onSlide.bind(this)} style={{cursor: 'pointer'}}>Slide</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,11 +1,53 @@
 import React from 'react'
 
+const styles = {
+    transition: 'all 1s ease-in'
+}
 export class Home extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            scale: 1,
+            opactiy: 1
+        }
+    }
+
+    onScale(){
+        this.setState({
+            scale: this.state.scale > 1 ? 1 : 1.3
+        })
+    }
+
+    onHide(){
+        this.setState({
+            opacity: 0
+        })
+    }
+
     render(){
         return (
-            <div>
-                <h3>Home</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="s12">
+
+                    </div>
+                </div>
+                <div className="row">
+                    <div className=".s8 offset-s2 center align">
+                        <div className="card deep-purple z-depth-2"
+                             style={{...styles, opacity: this.state.opacity, transform: 'scale('+this.state.scale+')'}}>
+                        <div className="card-content white-text">
+                            <span className="card-title">Home</span>
+                            <p>CSS Animation</p>
+                        </div>
+                        <div className="card-action">
+                            <a onClick={this.onHide.bind(this)} style={{cursor: 'pointer'}}>Hide</a>
+                            <a onClick={this.onScale.bind(this)} style={{cursor: 'pointer'}}>Scale</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        )
+    </div>
+    )
     }
 }
