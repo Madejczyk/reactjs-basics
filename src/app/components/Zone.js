7af26d45200e0a0067890ce85326a8001696f937
= <jakub.madejczyk92@gmail.com>
2017-07-21 00:50:38 +0100

    ReactCSSTransitionGroup part II

---
+++ src/app/components/Zone.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
+const styles = {
+    transition: 'all 1s ease-out'
+}
+export class Zone extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            list: ['Item 1', 'Item 2', 'Item 3']
+        }
+    }
+
+    onAddItem(){
+        let newElement = "Item " + (this.state.list.length + 1)
+        let updatedList = Object.assign([], this.state.list)
+        updatedList.push(newElement)
+        this.setState({
+            list: updatedList
+        })
+    }
+
+    onDeleteItem(id){
+        let updatedList = Object.assign([], this.state.list)
+        updatedList.splice(id, 1)
+        this.setState({
+            list: updatedList
+        })
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="s12">
+
+                    </div>
+                </div>
+                <div className="row">
+                    <div className=".s8 offset-s2 center align">
+                        <a className="waves-effect waves-light btn" onClick={this.onAddItem.bind(this)}>Add item</a>
+                        <p>Click item to Delete</p>
+                        <ul className="collection">
+                            <ReactCSSTransitionGroup
+                                transitionName='flipzone'
+                                transitionEnterTimeout={300}
+                                transitionLeaveTimeout={300}
+                                transitionAppear={true}
+                                transitionAppearTimeout={1000}>
+                                {this.state.list.map((item, i) => {
+                                    return (
+                                        <li key={item} className="collection-item"
+                                            onClick={this.onDeleteItem.bind(this, i)}
+                                            style={{cursor:'pointer'}}>{item}</li>
+                                    )
+                                })}
+                            </ReactCSSTransitionGroup>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import {Home} from './components/Home'
 import {Flight} from "./components/Flight"
 import {Alert} from './components/Alert'
+import {Zone} from './components/Zone'
 
 class App extends React.Component {
     render(){
@@ -15,6 +16,7 @@ class App extends React.Component {
                         <li><Link to="/home">Home</Link></li>
                         <li><Link to="/flight">Flight</Link></li>
                         <li><Link to="/alert">Alert</Link></li>
+                        <li><Link to="/zone">Zone</Link></li>
                     </ul>
 
                     <hr/>
@@ -22,6 +24,7 @@ class App extends React.Component {
                     <Route path="/home" component={Home}/>
                     <Route path="/flight" component={Flight}/>
                     <Route path="/alert" component={Alert}/>
+                    <Route path="/zone" component={Zone}/>
                 </div>
             </Router>
         )
