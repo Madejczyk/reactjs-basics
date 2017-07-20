7b3c46c366f2d4ff10399886ce12a606933624b9
= <jakub.madejczyk92@gmail.com>
2017-07-21 00:31:05 +0100

    ReactCSSTransitionGroup

--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import {Home} from './components/Home'
 import {Flight} from "./components/Flight"
+import {Alert} from './components/Alert'
 
 class App extends React.Component {
     render(){
@@ -13,12 +14,14 @@ class App extends React.Component {
                     <ul>
                         <li><Link to="/home">Home</Link></li>
                         <li><Link to="/flight">Flight</Link></li>
+                        <li><Link to="/alert">Alert</Link></li>
                     </ul>
 
                     <hr/>
                     <Route exact path="/" component={Home}/>
                     <Route path="/home" component={Home}/>
                     <Route path="/flight" component={Flight}/>
+                    <Route path="/alert" component={Alert}/>
                 </div>
             </Router>
         )
